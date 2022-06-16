87306b26b2bc71f4a7f1c382d8328098c6d4c36f
DW Kim <endix1029@gmail.com>
2022-06-16 14:32:32 +0900

    input flexibility

--- practice/main.c
+++ practice/main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hw17.h"
 
-void hw17(){
-    
+#define HWNO 17
+
+void hw17(int q, char** args){
+    char* ptr;
+    switch(q){
+        case 1:
+            P1();
+            break;
+        case 2:
+            P2();
+            break;
+        case 3:
+            for(int i = 1; i <= strtol(args[0], &ptr, 10); i++){
+                printf("fib(%d) = %d\n", i, P3(i));
+            }
+            break;
+        case 4:
+            for(int i = 1; i <= strtol(args[0], &ptr, 10); i++){
+                printf("%d_(4) = %d\n", i, P4(i));
+            };
+            break;
+        default: return;
+    }
 }
 
 int main() {
-    //P1();
-    //P2();
-    //for(int i = 0; i < 11; i++) printf("fib(%d) = %d\n", i, P3(i));
-    for(int i = 0; i < 11; i++) printf("%d_(4) = %d\n", i, P4(i));
+    char* arg = "10";
+    char* args[] = {arg};
+
+    switch(HWNO){
+        case 17: hw17(4, args); break;
+
+    }
     return 0;
 }
